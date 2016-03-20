6201ad5d56202592edea7bc75f31ca5db01386e8
Nick Olds <nolds9@yahoo.com>
2016-03-20 17:53:45 -0400

    adds avatar component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,63 @@
+/* React components should be FIRST
+
+Focused
+Independent
+Reusable
+Small
+Testable
+*/
+
+//////////////////////////////////////////////////////////////
+var USER_DATA = {
+  name: 'Nick Olds',
+  username: 'nolds9',
+  image: 'https://avatars.githubusercontent.com/u/10698044?v=3'
+}
+////////////////////////////////////////////////////////////
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function(){
     return (
-      <p>Jello World</p>
+      <img src={this.props.photoUrl} style={{height: 100, width: 100}} />
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic photoUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDom.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 )
